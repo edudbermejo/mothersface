7153f9829334cf221e3f5257a680b38b03889dcf
Eduardo David González <edudbermejo@gmail.com>
2019-08-30 01:39:26 +0100

    feat: it's alive. It does nothing but it's alive

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './nostromo.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>MU/TH/UR</h1>
+        <h2>6000</h2>
+        <img src={logo} className="App-logo" />
       </header>
+      <main class="App-main">
+        <div class="chat-container">
+          <span class="cursor"></span>
+        </div>
+      </main>
     </div>
   );
 }
